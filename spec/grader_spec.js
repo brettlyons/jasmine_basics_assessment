0d108f40137a6893ebf20339ad6a520cecb939c3
Brett Lyons <blyons@students.naropa.edu>
2015-08-10 09:21:38 -0600

    Refined test descriptions, made sure all tests are set to run

--- spec/grader_spec.js
+++ spec/grader_spec.js
@@ -8,31 +8,31 @@ describe('graderTests', function() {
   });
 });
 
-describe('letterGrader weird input', function() {
-  it('letterGrader( score ) is run with a string, not a score', function () {
+describe('graderTests', function() {
+  it('letterGrader( score ) is run and returns an error msg', function () {
     expect(code.letterGrader("lizard")).toEqual("lizard not a valid score");
   });
 });
 
-describe('mean Test', function() {
+describe('avgTests', function() {
   it('averageGrade( array ) is run and returns a mean score', function () {
     expect(code.averageGrade([90, 95, 87, 60])).toEqual("83");
   });
 });
 
-describe('median Test', function() {
+describe('avgTests', function() {
   it('medianGrade( array ) is run and returns a median score of the scores array', function () {
     expect(code.medianGrade([52,80,80,86,94])).toEqual("80");
   });
 });
 
-describe('mode Test', function() {
+describe('avgTests', function() {
   it('modeGrade( array ) is run and returns a mode of the scores array', function () {
     expect(code.modeGrade([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual("92");
   });
 })
 
-describe('mode Test', function() {
+describe('avgTests', function() {
   it('modeGrade( array ) is run and returns a mode of the array', function () {
     expect(code.modeGrade([82, 92, 82, 91, 92, 89, 82, 100, 86])).toEqual("82");
   });
